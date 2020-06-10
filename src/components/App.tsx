218e6867fc3a2220c5c4dcf67e1c93416fdb2959
Kary Ma <karyma101@hotmail.com>
2020-06-10 10:06:49 -0700

    added globals.d.ts file to remove warnings from imports

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,10 @@
-import React, {Component} from 'react'
-import * as ReactMarkdown from 'react-markdown'
+import React, { Component } from 'react'
+import ReactMarkdown from 'react-markdown'
 import source from '../content/test.md'
 
 class App extends Component {
-    render(){
-        return (
-            <ReactMarkdown source={source} />
-        )
+    render() {
+        return <ReactMarkdown source={source} />
     }
 }
 
