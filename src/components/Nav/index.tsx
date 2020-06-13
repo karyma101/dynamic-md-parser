30064fba113c7d3259b91b2d1c5c581d03737b1a
Kary Ma <karyma101@hotmail.com>
2020-06-13 05:48:47 -0700

    added basic layout and markdown styles

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -2,11 +2,13 @@ import React, { Fragment } from 'react'
 import { Link } from 'react-router-dom'
 import content from '../../content'
 
+import style from './Nav.css'
+
 const Nav = () => {
     const links = Object.keys(content)
 
     const renderLinks = links.map((link, i) => (
-        <Link key={`link_${i}`} to={`/${link}`}>
+        <Link key={`link_${i}`} to={`/${link}`} className={style.link}>
             {link}
         </Link>
     ))
